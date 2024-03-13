ca113c6f86ea7e8726e084fdce99c5000df66dd3
Mai Bao Ngoc <104448125+maibaongoc@users.noreply.github.com>
2024-03-14 00:06:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-alert("Xin chao cac ban");
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Toi rat thich tran anh thu</h1>
+
+    <h1>Toi hua se hoc that tot va co cong viec on dinh de lo cho gd va cau</h1>
+    <h1>Toi xin hua</h1>
+    <h3>Toi nho tran ngoc anh thu, rat nho</h3>
+    alert("Xin chao cac ban")
+
+</body>
+</html>
